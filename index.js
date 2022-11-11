a98155a8247968e4ec09bb1cd9ec8c06dbe63c70
Wilder Leon <wleona@ripley.com>
2022-11-11 14:16:08 -0300

    feat: Add express server

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-console.log('hola mundo!!!')
+import express from 'express'
+
+const app = express()
+
+app.get('/', (req, res) => {
+  res.json({
+    ok: true,
+  })
+})
+
+const port = 5000
+
+app.listen(port, () => console.log(`Server running in port ${port}`))
