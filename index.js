3f0c012619867e0f6befa499ff7c189dd29cfa35
Wilder Leon <wleona@ripley.com>
2022-11-11 14:28:28 -0300

    feat: Add dotenv & public directory

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 import express from 'express'
+import dotenv from 'dotenv'
 
+// Initializations
 const app = express()
+dotenv.config()
+
+// Public directory
+app.use(express.static('public'))
 
 app.get('/', (req, res) => {
   res.json({
@@ -8,6 +14,6 @@ app.get('/', (req, res) => {
   })
 })
 
-const port = 5000
+const port = process.env.PORT || 5000
 
 app.listen(port, () => console.log(`Server running in port ${port}`))
