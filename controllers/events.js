85b8809eb4fc46382b837eaddd95b8d350a2f0a7
Wilder Leon <wleona@ripley.com>
2022-11-18 17:23:52 -0300

    feat: Add event model

--- controllers/events.js
+++ controllers/events.js
@@ -14,6 +14,8 @@ export const getEvents = (req, res) => {
  * @desc Creates an event
  */
 export const createEvent = (req, res) => {
+  console.log(req.body)
+
   res.status(200).json({
     ok: true,
     msg: 'create event',
